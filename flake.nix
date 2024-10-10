9bfbc058414dcf00d4220f3d593dfdf016090322
Brian Li <me@brian14708.dev>
2024-10-10 19:37:12 +0800

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,45 @@
+{
+  description = "Nix configuration";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      home-manager,
+    }@inputs:
+    let
+      systems = [
+        "aarch64-linux"
+        "x86_64-linux"
+        "aarch64-darwin"
+      ];
+      pkgsFor = nixpkgs.lib.genAttrs (systems) (
+        system:
+        import nixpkgs {
+          inherit system;
+          config.allowUnfree = true;
+        }
+      );
+      forEachSystem = f: nixpkgs.lib.genAttrs (systems) (system: f pkgsFor.${system});
+    in
+    {
+      formatter = forEachSystem (pkgs: pkgs.nixfmt-rfc-style);
+      devShells = forEachSystem (pkgs: import ./shell.nix { inherit pkgs; });
+
+      homeConfigurations = {
+        "brian" = home-manager.lib.homeManagerConfiguration {
+          pkgs = pkgsFor.x86_64-linux;
+          modules = [ ./home.nix ];
+          extraSpecialArgs = { };
+        };
+      };
+    };
+}
---
+++ home.nix
@@ -0,0 +1,45 @@
+{ config, pkgs, ... }:
+{
+  home.username = "brian";
+  home.homeDirectory =
+    if pkgs.stdenv.isDarwin then "/Users/${config.home.username}" else "/home/${config.home.username}";
+
+  home.stateVersion = "24.05";
+
+  home.packages = [
+    pkgs.devenv
+  ];
+
+  home.file.".ssh/authorized_keys" = {
+    source = pkgs.fetchurl {
+      url = "https://brian14708.dev/ssh-key";
+      hash = "sha256-4rgJi2M8DUyuD6xB91ZMUe6TgdMzEdXqtL4A/6ZOXSo=";
+    };
+  };
+
+  programs = {
+    home-manager.enable = true;
+    git = {
+      enable = true;
+      userName = "Brian Li";
+      userEmail = "me@brian14708.dev";
+      difftastic.enable = true;
+    };
+  };
+
+  nix = {
+    package = pkgs.nix;
+    settings = {
+      experimental-features = [
+        "nix-command"
+        "flakes"
+      ];
+      substituters = [
+        "https://mirrors.cernet.edu.cn/nix-channels/store"
+        "https://mirror.sjtu.edu.cn/nix-channels/store"
+        "https://cache.nixos.org"
+      ];
+    };
+  };
+
+}
