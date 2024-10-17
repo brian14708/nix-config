9c0568e437467694a20cb5c650c712ba8b0ee65e
Brian Li <me@brian14708.dev>
2024-10-17 14:55:29 +0800

    Add machine trust

--- home/brian/global/default.nix
+++ home/brian/global/default.nix
@@ -3,6 +3,7 @@
   pkgs,
   lib,
   inputs,
+  machine,
   ...
 }:
 {
@@ -23,32 +24,35 @@
     age.sshKeyPaths = [ "${config.home.homeDirectory}/.ssh/id_ed25519" ];
   };
 
-  sops.secrets = {
+  sops.secrets = lib.optionalAttrs machine.trusted {
     "nix.conf" = {
       sopsFile = ../secrets/global.nix.yaml;
     };
   };
 
-  nix = {
-    package = lib.mkDefault pkgs.nixVersions.latest;
-    settings = {
-      use-xdg-base-directories = true;
-      experimental-features = [
-        "nix-command"
-        "flakes"
-      ];
-      substituters = [
-        "https://nix-community.cachix.org"
-        "https://mirrors.cernet.edu.cn/nix-channels/store"
-        "https://mirror.sjtu.edu.cn/nix-channels/store"
-        "https://cache.nixos.org"
-      ];
-      trusted-public-keys = [
-        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-        "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
-      ];
+  nix =
+    {
+      package = lib.mkDefault pkgs.nixVersions.latest;
+      settings = {
+        use-xdg-base-directories = true;
+        experimental-features = [
+          "nix-command"
+          "flakes"
+        ];
+        substituters = [
+          "https://nix-community.cachix.org"
+          "https://mirrors.cernet.edu.cn/nix-channels/store"
+          "https://mirror.sjtu.edu.cn/nix-channels/store"
+          "https://cache.nixos.org"
+        ];
+        trusted-public-keys = [
+          "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+          "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+        ];
+      };
+    }
+    // lib.optionalAttrs machine.trusted {
+      extraOptions = "include ${config.sops.secrets."nix.conf".path}\n";
+      checkConfig = false;
     };
-    extraOptions = "include ${config.sops.secrets."nix.conf".path}\n";
-    checkConfig = false;
-  };
 }
