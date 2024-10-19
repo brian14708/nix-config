b42305915369cbcbcc9ff8d35714ce07dfc4fd48
Brian Li <me@brian14708.dev>
2024-10-19 14:26:57 +0800

    Add impermanence

--- hosts/aether/default.nix
+++ hosts/aether/default.nix
@@ -6,13 +6,28 @@
 }:
 {
   imports = [
+    inputs.impermanence.nixosModules.impermanence
+    inputs.sops-nix.nixosModules.sops
     inputs.disko.nixosModules.disko
     ./disko.nix
   ];
 
   boot.kernelPackages = pkgs.linuxPackages_latest;
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
+  boot.loader = {
+    systemd-boot = {
+      enable = true;
+      configurationLimit = 5;
+    };
+    efi.canTouchEfiVariables = true;
+    timeout = 3;
+  };
+
+  sops = {
+    defaultSopsFile = ./secret.yaml;
+    age.sshKeyPaths = [ "/nix/persist/etc/ssh/ssh_host_ed25519_key" ];
+    secrets."brian-password".neededForUsers = true;
+    secrets."brian-password" = { };
+  };
 
   networking.hostName = "aether";
   networking.wireless.iwd.enable = true;
@@ -37,6 +52,7 @@
     openssh.authorizedKeys.keys = [
       "cert-authority,principals=\"me@brian14708.dev\" ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQDeS6dWCB0TwwnmL6ynrQuLr5jsqS0dwjuwgw3FLen9P1hg+PMhwyw2G7ABfogZHwNG5y2jvB5iLfclrKPDQ/B31oJeWMV5hilDIiTLPTtIqKd93QQujyyLUqznC3dYNzJC7vBr0HGcR6te90Fjk80vsfFUQ/kE3PVJVGguhZI9TX9T2JepOlyQ597NSNuNkx7GUG9vrdZwxkyC3PUu2ipyLOvmLTiRPgl0wLXoIHUTgt0GfM5KpF3tlSirrWBu9WFdfL37YDvQt7JhqmsIXuUusNRw95HlROTujjV5xgWmv59t7TIdWRO3M2wzNQ257Wd3TZXmoYyk5TSzLvIWXb9dW0KlK4u8xaK0CU/H4Ro30coWveujmCX3jAxfAFpCSDHsy79JX/MIi43HnLJjvBY+1/VCwKwGUyXajq8/5XOCdBYYcQcNzfvWPoA2j8VlkxgaMHQ7i5tUy2dAHzKdJDmfuSyDrHEzfgGpAna8NaRbH5WKMpxX7dmlgmI0kWOw1nojfC8CCJyfEYPS81b7m9Z65C0+m+zhruUY9A/v3MdmwHlnkMMFmLHaavJSxK1U1ROGs/MYEiauBZiYiFPXbJnDNrU7hujTwdXvO5adJO8oZ9byOazB09vnRNQgc/X6hIas2Fh13tQ8NMbqZGWLcmfH6LkdjrVloRbbV7QtU0GCGQ=="
     ];
+    hashedPasswordFile = config.sops.secrets."brian-password".path;
   };
 
   environment.systemPackages = with pkgs; [
@@ -73,4 +89,23 @@
       trusted-users = [ "@wheel" ];
     };
   };
+
+  environment.persistence."/nix/persist" = {
+    hideMounts = true;
+
+    directories = [
+      "/var/log"
+      "/var/lib/nixos"
+      "/var/lib/systemd/coredump"
+    ];
+
+    files = [
+      "/etc/machine-id"
+      "/etc/ssh/ssh_host_ed25519_key.pub"
+      "/etc/ssh/ssh_host_ed25519_key"
+      "/etc/ssh/ssh_host_rsa_key.pub"
+      "/etc/ssh/ssh_host_rsa_key"
+    ];
+  };
+  fileSystems."/nix/persist".neededForBoot = true;
 }
