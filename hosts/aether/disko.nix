f8d35ca09df3521abbfb94b443b9794000afbd9d
Brian Li <me@brian14708.dev>
2024-10-19 14:26:57 +0800

    Add impermanence

--- hosts/aether/disko.nix
+++ hosts/aether/disko.nix
@@ -1,62 +1,74 @@
 {
-  disko.devices.disk.nvme = {
-    type = "disk";
-    device = "/dev/nvme0n1";
-    content = {
-      type = "gpt";
-      partitions = {
-        ESP = {
-          type = "EF00";
-          size = "512M";
-          content = {
-            type = "filesystem";
-            format = "vfat";
-            mountpoint = "/boot";
-            mountOptions = [ "umask=0077" ];
-          };
-        };
-        root = {
-          size = "100%";
-          content = {
-            type = "luks";
-            name = "root";
-            settings = {
-              allowDiscards = true;
-            };
+  disko.devices = {
+    disk.nvme = {
+      type = "disk";
+      device = "/dev/nvme0n1";
+      content = {
+        type = "gpt";
+        partitions = {
+          ESP = {
+            type = "EF00";
+            size = "512M";
             content = {
-              type = "btrfs";
-              subvolumes = {
-                "@" = { };
-                "@/root" = {
-                  mountpoint = "/";
-                  mountOptions = [
-                    "compress=zstd"
-                    "noatime"
-                  ];
-                };
-                "@/nix" = {
-                  mountpoint = "/nix";
-                  mountOptions = [
-                    "compress=zstd"
-                    "noatime"
-                  ];
-                };
-                "@/home" = {
-                  mountpoint = "/home";
-                  mountOptions = [
-                    "compress=zstd"
-                    "noatime"
-                  ];
-                };
-                "@/swap" = {
-                  mountpoint = "/nix/swap";
-                  swap.swapfile.size = "32G";
+              type = "filesystem";
+              format = "vfat";
+              mountpoint = "/boot";
+              mountOptions = [ "umask=0077" ];
+            };
+          };
+          root = {
+            size = "100%";
+            content = {
+              type = "luks";
+              name = "root";
+              settings = {
+                allowDiscards = true;
+              };
+              content = {
+                type = "btrfs";
+                subvolumes = {
+                  "@" = { };
+                  "@/nix" = {
+                    mountpoint = "/nix";
+                    mountOptions = [
+                      "compress=zstd"
+                      "noatime"
+                    ];
+                  };
+                  "@/persist" = {
+                    mountpoint = "/nix/persist";
+                    mountOptions = [
+                      "compress=zstd"
+                      "noatime"
+                      "noexec"
+                    ];
+                  };
+                  "@/swap" = {
+                    mountpoint = "/nix/swap";
+                    swap.swapfile.size = "32G";
+                  };
                 };
               };
             };
           };
         };
       };
+    };
+    nodev."/" = {
+      fsType = "tmpfs";
+      mountOptions = [
+        "size=2G"
+        "defaults"
+        "mode=755"
+      ];
+    };
+    nodev."/home" = {
+      fsType = "tmpfs";
+      mountOptions = [
+        "size=2G"
+        "defaults"
+        "mode=777"
+      ];
     };
   };
 }
