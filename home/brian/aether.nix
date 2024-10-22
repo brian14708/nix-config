63201e4b5e90225ddf58ea9016ab73336c46eb95
Brian Li <me@brian14708.dev>
2024-10-23 00:21:24 +0800

    Update for aether

--- home/brian/aether.nix
+++ home/brian/aether.nix
@@ -1,12 +1,14 @@
 {
   config,
   pkgs,
+  inputs,
   ...
 }:
 {
   imports = [
     ./common
   ];
+
   home = {
     username = "brian";
     stateVersion = "24.11";
@@ -21,57 +23,146 @@
       noto-fonts-cjk-sans
       noto-fonts-cjk-serif
       noto-fonts-color-emoji
-
-      tofi
     ];
   };
+  programs.gpg = {
+    enable = true;
+  };
+  services.gpg-agent = {
+    enable = true;
+  };
+  home.pointerCursor = {
+    name = "Vimix-Cursors";
+    package = pkgs.vimix-cursor-theme;
+  };
+
   programs.bash.enable = true;
+  programs.fuzzel = {
+    enable = true;
+  };
   wayland.windowManager.hyprland = {
     enable = true;
     xwayland.enable = true;
     settings = {
+      exec-once = [
+        "${pkgs.foot}/bin/foot --server"
+        "${pkgs.waybar}/bin/waybar"
+      ];
+      monitor = ",preferred,auto,auto";
       "$mod" = "SUPER";
       bind = [
-        "$mod, Return, exec, footclient -N || foot"
-        "$mod, Space, exec, tofi-run | xargs hyprctl dispatch exec --"
+        "$mod, Return, exec, ${pkgs.foot}/bin/footclient -N || ${pkgs.foot}/bin/foot"
+        "$mod, Space, exec, ${pkgs.fuzzel}/bin/fuzzel"
+        "$mod, Tab, layoutmsg, swapwithmaster"
+        "$mod, f, fullscreen, 1"
+        "$mod, q, killactive"
+        "$mod CONTROL, q, exit"
+
+        # workspace
+        "$mod, 1, workspace, 1"
+        "$mod, 2, workspace, 2"
+        "$mod, 3, workspace, 3"
+        "$mod, 4, workspace, 4"
+        "$mod, 5, workspace, 5"
+        "$mod, 6, workspace, 6"
+        "$mod, 7, workspace, 7"
+        "$mod, 8, workspace, 8"
+        "$mod, 9, workspace, 9"
+        "$mod, 0, workspace, 0"
+
+        "$mod SHIFT, 1, movetoworkspace, 1"
+        "$mod SHIFT, 2, movetoworkspace, 2"
+        "$mod SHIFT, 3, movetoworkspace, 3"
+        "$mod SHIFT, 4, movetoworkspace, 4"
+        "$mod SHIFT, 5, movetoworkspace, 5"
+        "$mod SHIFT, 6, movetoworkspace, 6"
+        "$mod SHIFT, 7, movetoworkspace, 7"
+        "$mod SHIFT, 8, movetoworkspace, 8"
+        "$mod SHIFT, 9, movetoworkspace, 9"
+        "$mod SHIFT, 0, movetoworkspace, 0"
       ];
+      binde = [
+        "$mod, h, resizeactive, -50 0"
+        "$mod, l, resizeactive, 50 0"
+        "$mod SHIFT, h, resizeactive, -5 0"
+        "$mod SHIFT, l, resizeactive, 5 0"
+        "$mod, k, layoutmsg, cycleprev"
+        "$mod, j, layoutmsg, cyclenext"
+        "$mod SHIFT, k, layoutmsg, swapprev"
+        "$mod SHIFT, j, layoutmsg, swapnext"
+      ];
+      input = {
+        kb_options = "ctrl:nocaps,altwin:swap_alt_win";
+        repeat_delay = 250;
+        touchpad = {
+          natural_scroll = true;
+          scroll_factor = 0.175;
+        };
+      };
+      general = {
+        gaps_in = 5;
+        gaps_out = 10;
+        border_size = 1;
+        layout = "master";
+        "col.active_border" = "rgb(999999)";
+        "col.inactive_border" = "rgb(333333)";
+      };
+      animations.enabled = false;
+      master = {
+        new_status = "master";
+        no_gaps_when_only = true;
+        mfact = 0.6;
+      };
+      misc = {
+        disable_hyprland_logo = true;
+        disable_splash_rendering = true;
+      };
+      xwayland = {
+        force_zero_scaling = true;
+      };
     };
+  };
+
+  programs.zoxide = {
+    enable = true;
+    enableBashIntegration = true;
+  };
+  programs.waybar = {
+    enable = true;
   };
 
   programs.foot = {
     enable = true;
-    server.enable = true;
     settings = {
-      font = "code";
-      fontSize = 10;
+      main = {
+        font = "monospace:size=10";
+      };
     };
   };
 
   fonts.fontconfig = {
     enable = true;
     defaultFonts = {
-      sans = [
+      sansSerif = [
         "Inter"
         "Noto Sans"
-        "Noto Color Emoji"
         "Noto Sans CJK SC"
       ];
       serif = [
         "Noto Serif"
         "Noto Serif CJK SC"
       ];
-      code = [
-        "CaskaydiaCove Nerd Font"
+      emoji = [
         "Noto Color Emoji"
-        "Noto Sans Mono"
-        "Noto Sans Mono CJK SC"
       ];
       monospace = [
+        "CaskaydiaCove Nerd Font"
         "Noto Sans Mono"
         "Noto Sans Mono CJK SC"
       ];
     };
   };
+  home.sessionVariables.NIXOS_OZONE_WL = "1";
 
   xdg.userDirs = {
     enable = true;
@@ -84,5 +175,21 @@
     desktop = "${config.home.homeDirectory}/public";
     publicShare = "${config.home.homeDirectory}/public";
     templates = "${config.home.homeDirectory}/public";
+  };
+  i18n.inputMethod = {
+    enabled = "fcitx5";
+    fcitx5.addons = with pkgs; [
+      fcitx5-rime
+      rime-data
+      fcitx5-tokyonight
+    ];
+  };
+  dconf = {
+    enable = true;
+    settings = {
+      "org/gnome/desktop/interface" = {
+        color-scheme = "prefer-dark";
+      };
+    };
   };
 }
--- hosts/aether/default.nix
+++ hosts/aether/default.nix
@@ -16,7 +16,7 @@
   ];
 
   boot = {
-    kernelPackages = pkgs.linuxPackages_latest;
+    kernelPackages = pkgs.linuxPackages_xanmod_latest;
     kernelParams = [ "mitigations=off" ];
     initrd.systemd.enable = true;
     loader = {
@@ -63,7 +63,9 @@
     uid = 1000;
     description = "Brian";
     isNormalUser = true;
-    extraGroups = [ "wheel" ];
+    extraGroups = [
+      "wheel"
+    ];
     openssh.authorizedKeys.keys = [
       "cert-authority,principals=\"me@brian14708.dev\" ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQDeS6dWCB0TwwnmL6ynrQuLr5jsqS0dwjuwgw3FLen9P1hg+PMhwyw2G7ABfogZHwNG5y2jvB5iLfclrKPDQ/B31oJeWMV5hilDIiTLPTtIqKd93QQujyyLUqznC3dYNzJC7vBr0HGcR6te90Fjk80vsfFUQ/kE3PVJVGguhZI9TX9T2JepOlyQ597NSNuNkx7GUG9vrdZwxkyC3PUu2ipyLOvmLTiRPgl0wLXoIHUTgt0GfM5KpF3tlSirrWBu9WFdfL37YDvQt7JhqmsIXuUusNRw95HlROTujjV5xgWmv59t7TIdWRO3M2wzNQ257Wd3TZXmoYyk5TSzLvIWXb9dW0KlK4u8xaK0CU/H4Ro30coWveujmCX3jAxfAFpCSDHsy79JX/MIi43HnLJjvBY+1/VCwKwGUyXajq8/5XOCdBYYcQcNzfvWPoA2j8VlkxgaMHQ7i5tUy2dAHzKdJDmfuSyDrHEzfgGpAna8NaRbH5WKMpxX7dmlgmI0kWOw1nojfC8CCJyfEYPS81b7m9Z65C0+m+zhruUY9A/v3MdmwHlnkMMFmLHaavJSxK1U1ROGs/MYEiauBZiYiFPXbJnDNrU7hujTwdXvO5adJO8oZ9byOazB09vnRNQgc/X6hIas2Fh13tQ8NMbqZGWLcmfH6LkdjrVloRbbV7QtU0GCGQ=="
     ];
@@ -119,4 +121,13 @@
   };
 
   programs.hyprland.enable = true;
+  services.greetd = {
+    enable = true;
+    settings = {
+      default_session = {
+        command = "${pkgs.greetd.tuigreet}/bin/tuigreet --time";
+        user = "greeter";
+      };
+    };
+  };
 }
