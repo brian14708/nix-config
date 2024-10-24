6a21b25b0f95f946d0e68902f571600b7157d0d9
Brian Li <me@brian14708.dev>
2024-10-24 23:44:28 +0800

    Fix fcitx5

--- modules/home/desktop/fcitx5/default.nix
+++ modules/home/desktop/fcitx5/default.nix
@@ -32,6 +32,7 @@ in
     };
   };
   xdg.configFile."fcitx5/profile" = {
+    force = true;
     text = lib.generators.toINI { } {
       "Groups/0" = {
         Name = "Default";
@@ -40,12 +41,65 @@ in
       };
       "Groups/0/Items/0" = {
         Name = "keyboard-us";
+        Layout = "";
       };
       "Groups/0/Items/1" = {
         Name = "rime";
+        Layout = "";
       };
       "GroupOrder" = {
         "0" = "Default";
+      };
+    };
+  };
+  xdg.configFile."fcitx5/config" = {
+    text = lib.generators.toINI { } {
+      "Hotkey" = {
+        EnumerateWithTriggerKeys = "True";
+        EnumerateForwardKeys = "";
+        EnumerateBackwardKeys = "";
+        EnumerateSkipFirst = "False";
+      };
+      "Hotkey/TriggerKeys" = {
+        "0" = "Control+space";
+      };
+      "Hotkey/AltTriggerKeys" = {
+        "0" = "Shift_L";
+      };
+      "Hotkey/EnumerateForwardKeys" = {
+        "0" = "Control+Shift_L";
+      };
+      "Hotkey/EnumerateBackwardKeys" = {
+        "0" = "Control+Shift_R";
+      };
+      "Hotkey/PrevPage" = {
+        "0" = "Up";
+      };
+      "Hotkey/NextPage" = {
+        "0" = "Down";
+      };
+      "Hotkey/PrevCandidate" = {
+        "0" = "Shift+Tab";
+      };
+      "Hotkey/NextCandidate" = {
+        "0" = "Tab";
+      };
+      "Hotkey/TogglePreedit" = {
+        "0" = "Control+Alt+P";
+      };
+      "Behavior" = {
+        "ActiveByDefault" = "False";
+        "ShareInputState" = "No";
+        "PreeditEnabledByDefault" = "True";
+        "ShowInputMethodInformation" = "True";
+        "showInputMethodInformationWhenFocusIn" = "False";
+        "CompactInputMethodInformation" = "True";
+        "ShowFirstInputMethodInformation" = "True";
+        "DefaultPageSize" = 5;
+        "OverrideXkbOption" = "False";
+        "CustomXkbOption" = "";
+        "EnabledAddons" = "";
+        "PreloadInputMethod" = "True";
       };
     };
   };
