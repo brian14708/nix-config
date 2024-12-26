a152c4cc2d4eddfee6b9bed5b15889072b71fa63
Brian Li <me@brian14708.dev>
2024-12-26 22:29:27 +0800

    Jupyter template

--- templates/default.nix
+++ templates/default.nix
@@ -4,4 +4,8 @@
     path = ./rust;
     description = "Rust application";
   };
+  jupyter = {
+    path = ./jupyter;
+    description = "Jupyter notebook";
+  };
 }
---
+++ templates/jupyter/flake.nix
@@ -0,0 +1,44 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+  };
+
+  outputs =
+    inputs@{ flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "aarch64-darwin"
+        "x86_64-darwin"
+      ];
+      perSystem =
+        {
+          config,
+          self',
+          inputs',
+          pkgs,
+          system,
+          ...
+        }:
+        {
+
+          devShells.default = pkgs.mkShell {
+            venvDir = ".venv";
+            packages =
+              with pkgs;
+              [
+                uv
+                python312
+              ]
+              ++ (with python312Packages; [
+                venvShellHook
+                pip
+                jupyter
+                numpy
+              ]);
+          };
+        };
+    };
+}
