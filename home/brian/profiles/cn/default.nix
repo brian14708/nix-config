028972932482665110020d6e26ea4efab876c985
Brian Li <me@brian14708.dev>
2024-12-30 13:11:55 +0800

    Add cn proxy

---
+++ home/brian/profiles/cn/default.nix
@@ -0,0 +1,40 @@
+{
+  config,
+  ...
+}:
+let
+  home = config.home.homeDirectory;
+in
+{
+  home.file = {
+    ".cargo/config.toml" = {
+      text = ''
+        [source.crates-io]
+        replace-with = 'rsproxy-sparse'
+        [source.rsproxy]
+        registry = "https://rsproxy.cn/crates.io-index"
+        [source.rsproxy-sparse]
+        registry = "sparse+https://rsproxy.cn/index/"
+        [registries.rsproxy]
+        index = "https://rsproxy.cn/crates.io-index"
+        [net]
+        git-fetch-with-cli = true
+      '';
+    };
+  };
+  xdg.configFile."go/env" = {
+    text = ''
+      GOPATH=${home}/.local/go
+      GOPROXY=https://goproxy.cn,direct
+    '';
+  };
+  nix = {
+    settings = {
+      substituters = [
+        "https://mirrors.tuna.tsinghua.edu.cn/nix-channels/store"
+        "https://mirror.sjtu.edu.cn/nix-channels/store"
+        "https://cache.nixos.org"
+      ];
+    };
+  };
+}
