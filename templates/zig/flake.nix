7d7b3827b743e1f895c2e73c1c98069b42f04497
Brian Li <me@brian14708.dev>
2025-01-18 15:00:02 +0800

    Add zig gitignore and formatter

--- templates/zig/flake.nix
+++ templates/zig/flake.nix
@@ -2,11 +2,18 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     flake-parts.url = "github:hercules-ci/flake-parts";
+    treefmt-nix = {
+      url = "github:numtide/treefmt-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs =
     inputs@{ flake-parts, ... }:
     flake-parts.lib.mkFlake { inherit inputs; } {
+      imports = [
+        inputs.treefmt-nix.flakeModule
+      ];
       systems = [
         "x86_64-linux"
         "aarch64-linux"
@@ -23,12 +30,15 @@
           ...
         }:
         {
-
           devShells.default = pkgs.mkShell {
             packages = with pkgs; [
               zig
               zls
             ];
+          };
+          treefmt.programs = {
+            zig.enable = true;
+            nixfmt.enable = true;
           };
         };
     };
