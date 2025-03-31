71919f5b93eb1caadb983af60349c7df4d96d4a9
Brian Li <me@brian14708.dev>
2025-03-31 18:52:32 +0800

    Add route

--- hosts/workstation/shiva/default.nix
+++ hosts/workstation/shiva/default.nix
@@ -50,7 +50,7 @@
   services.tailscale = {
     useRoutingFeatures = "server";
     extraSetFlags = [
-      "--advertise-routes=fd7a:115c:a1e0:b1a:0:1:a00:0/104"
+      "--advertise-routes=fd7a:115c:a1e0:b1a:0:1:a00:0/104,fd7a:115c:a1e0:b1a:0:1:6440:0/106"
     ];
   };
   virtualisation.containerd = {
