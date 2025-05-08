361a11f9a630e896987a98c13a949948a7b98d90
Brian Li <me@brian14708.dev>
2025-05-08 09:26:26 +0800

    Use iwd as wifi backend

--- hosts/workstation/base-linux.nix
+++ hosts/workstation/base-linux.nix
@@ -21,7 +21,10 @@
   };
   systemd.coredump.enable = false;
   boot.kernel.sysctl."kernel.core_pattern" = "|/run/current-system/sw/bin/false";
-  networking.networkmanager.enable = true;
+  networking.networkmanager = {
+    enable = true;
+    wifi.backend = "iwd";
+  };
   networking.firewall.trustedInterfaces = [
     config.services.tailscale.interfaceName
   ];
