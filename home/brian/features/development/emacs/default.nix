4345f019bec29ce4a3189fb10a7af046cd83fd65
Brian Li <me@brian14708.dev>
2025-05-14 14:57:01 +0800

    Fix emacs config

--- home/brian/features/development/emacs/default.nix
+++ home/brian/features/development/emacs/default.nix
@@ -5,11 +5,12 @@
 {
   programs.emacs = {
     enable = true;
-    package = pkgs.emacs30-pgtk;
+    package = pkgs.emacs-pgtk;
   };
   services.emacs = {
     enable = true;
     socketActivation.enable = true;
   };
   xdg.configFile."emacs/init.el".source = ./init.el;
+  xdg.configFile."emacs/early-init.el".source = ./early-init.el;
 }
---
+++ home/brian/features/development/emacs/early-init.el
@@ -0,0 +1,4 @@
+(setq default-frame-alist '(
+  (background-color . "#000000")
+  (ns-appearance . dark)
+  (ns-transparent-titlebar . t)))
