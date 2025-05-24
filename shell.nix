8f09046a4da5216c2c83ed60448534e0affa1958
Brian Li <me@brian14708.dev>
2025-05-24 18:57:48 +0800

    Add lsp to neovim

--- shell.nix
+++ shell.nix
@@ -14,15 +14,10 @@
         nixVersions.latest
         nix-update
         home-manager
-        git
         sops
         deploy-rs.deploy-rs
         just
         opentofu
-
-        nil
-        nixfmt-rfc-style
-        lua-language-server
       ]
       ++ (if pkgs.stdenv.isDarwin then [ darwin-rebuild ] else [ ]);
   };
